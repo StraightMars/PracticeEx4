1962592e75c1723ea38f68355e46cf89c0c0ab38
nureevmarsel <mnurev@mail.ru>
2018-06-16 12:47:40 +0500

    NothingSpecial

--- Practice4/Practice4/Program.cs
+++ Practice4/Practice4/Program.cs
@@ -10,7 +10,42 @@ namespace Practice4
     {
         static void Main(string[] args)
         {
-
+            int osn = 2;
+            int step = -200;
+            string s, tt;
+            int k, a, b, length;
+            s = "2";
+            tt = s;
+            k = 0;
+            a = (Convert.ToInt32(tt[0]) * osn + k);
+            Console.WriteLine(a);
+            //for (int i = -190; i > step; i--)
+            //{
+            //    tt = s;
+            //    length = tt.Length;
+            //    s = "";
+            //    k = 0;
+            //    for (int j = 0; j < length; j++)
+            //    {
+            //        a = (Convert.ToInt32(tt[j])) * osn + k;
+            //        b = a % 10;
+            //        k = a / 10;
+            //        s += Convert.ToString(b);
+            //        if (k != 0)
+            //        {
+            //            s += Convert.ToString(k);
+            //        }
+            //    }
+            //}
+            //char[] ch = s.ToCharArray();
+            //for (int i = 0; i < ch.Length / 2; i++)
+            //{
+            //    char tmp = ch[ch.Length - i - 1];
+            //    ch[ch.Length - i - 1] = ch[i];
+            //    ch[i] = tmp;
+            //}
+            //s = new string(ch);
+            ////Console.WriteLine(s);
         }
     }
 }
